1f695dbba9f849ff49ba5c1ba76c5026361ac19f
santukumar01 <santu13102000@gmail.com>
2025-07-09 13:07:16 +0530

    intial commit

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-require("dotenv").config(); // Load env variables
-
-const app = require("./Backend/src/app.js");
+const path = require("path");
+const express = require("express");
+const app = require("./Backend/src/app");
 
 const PORT = process.env.PORT || 3000;
 
+// Serve frontend
+app.use(express.static(path.join(__dirname, "Frontend", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
