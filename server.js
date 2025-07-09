0eb1cee76181e3d96a3d42928bd8625dc5f8948e
santukumar01 <santu13102000@gmail.com>
2025-07-09 13:17:20 +0530

    Final structure update

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const path = require("path");
 const express = require("express");
-const app = require("./Backend/src/app");
-
+const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
 
-// Serve frontend
-app.use(express.static(path.join(__dirname, "Frontend", "dist")));
+const frontendPath = path.join(__dirname, "Frontend", "dist");
 
+app.use(express.static(frontendPath));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+  res.sendFile(path.join(frontendPath, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
